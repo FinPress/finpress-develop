bcb73c59c16d0bf3eb3bfe44c0af5cdbeb2b8e72
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2025-04-08 10:21:44 +0530

    Simplify URL scheme handling in get_blogaddress_by_id using set_url_scheme()

--- src/wp-includes/ms-deprecated.php
+++ src/wp-includes/ms-deprecated.php
@@ -623,9 +623,7 @@ function install_blog( $blog_id, $blog_title = '' ) {
 	// Get the site URL directly from get_site() instead of using get_blogaddress_by_id().
 	$site_info = get_site( $blog_id );
 	if ( ! empty( $site_info ) ) {
-		$scheme = parse_url( $site_info->home, PHP_URL_SCHEME );
-		$scheme = empty( $scheme ) ? 'http' : $scheme;
-		$url    = esc_url( $scheme . '://' . $site_info->domain . $site_info->path );
+		$url = set_url_scheme( 'http://' . $site_info->domain . $site_info->path );
 	} else {
 		$url = '';
 	}
