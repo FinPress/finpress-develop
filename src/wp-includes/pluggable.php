ed0a2bce4267ae5413fc3e083b866ae00cd136a9
Rishabh Gupta <rishabh.gupta@rtcamp.com>
2025-08-11 12:58:21 +0530

    mail: reset PHPMailer encoding between wp_mail() calls

--- src/wp-includes/pluggable.php
+++ src/wp-includes/pluggable.php
@@ -358,6 +358,7 @@ if ( ! function_exists( 'wp_mail' ) ) :
 		$phpmailer->clearReplyTos();
 		$phpmailer->Body    = '';
 		$phpmailer->AltBody = '';
+		$phpmailer->Encoding = PHPMailer\PHPMailer\PHPMailer::ENCODING_8BIT;
 
 		// Set "From" name and email.
 
