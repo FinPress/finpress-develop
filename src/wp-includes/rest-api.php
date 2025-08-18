91b389e27ba443f2db7687b6b3f784550d413a85
Grzegorz Ziolkowski <grzegorz@gziolo.pl>
2025-08-18 11:36:39 +0200

    Fix order of abilities routes registration

--- src/wp-includes/rest-api.php
+++ src/wp-includes/rest-api.php
@@ -418,10 +418,10 @@ function create_initial_rest_routes() {
 	$font_collections_controller->register_routes();
 
 	// Abilities.
-	$abilities_list_controller = new WP_REST_Abilities_List_Controller();
-	$abilities_list_controller->register_routes();
 	$abilities_run_controller  = new WP_REST_Abilities_Run_Controller();
 	$abilities_run_controller->register_routes();
+	$abilities_list_controller = new WP_REST_Abilities_List_Controller();
+	$abilities_list_controller->register_routes();
 }
 
 /**
